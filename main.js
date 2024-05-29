569d5c182e3b8502dc373ba6578de7c80e36a877
KobayashiMauro <auroravalentinivalea@icloud.com>
2024-05-29 18:35:42 +0200

    Estoy teniendo problemas para añadir los <a> entre .moment y la imagen, de momento así funciona con los <a> comentados.

--- main.js
+++ main.js
@@ -22,6 +22,118 @@ const moments = [
     ],
   },
   {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
     moment: 'Law VS Kurohige',
     anime: 'One Piece',
     characters: [
@@ -43,13 +155,156 @@ const moments = [
       },
     ],
   },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
+  {
+    moment: 'El pasado de Law',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Trafalgar D. Water Law',
+        second: 'Donquixote Doflamingo',
+        third: 'Donquixote Rosinante Corazon',
+      },
+    ],
+    episodes: 8,
+    duration: 192,
+    description:
+      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
+    image: 'assets/Lawminicard2.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
+        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
+        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
+        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
+        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
+        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
+        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+      },
+    ],
+  },
 ];
-
-for (const moment of moments) {
+/* Print cards --------------------------------------------- */
+/* for (const moment of moments) {
   //Me traigo los datos y creo contenedores
   let momentName = document.createTextNode(moment.moment);
   let anime = document.createTextNode(moment.anime);
   let card = document.createElement('article');
+  let imgDiv = document.createElement('div');
+  let background = document.createElement('img');
+  background.src = moment.image;
+  let anchorImg = document.createElement('a');
+  let anchorH3 = document.createElement('a');
+  let anchorH4 = document.createElement('a');
+  let cardsDiv = document.querySelector('div.cards');
+  let cardH3 = document.createElement('h3');
+  let cardH4 = document.createElement('h4');
+  card.classList.add('moment');
+  imgDiv.classList.add('img');
+  anchorImg.href = '#';
+  anchorH3.href = '#';
+  anchorH4.href = '#';
+  //Pinto los datos
+  cardsDiv.appendChild(card);
+  card.appendChild(anchorImg);
+  card.appendChild(anchorH3);
+  card.appendChild(anchorH4);
+  anchorImg.appendChild(imgDiv);
+  imgDiv.appendChild(background);
+  cardH3.appendChild(momentName);
+  cardH4.appendChild(anime);
+  anchorH3.appendChild(cardH3);
+  anchorH4.appendChild(cardH4);
+} */
+for (const moment of moments) {
+  //Me traigo los datos y creo contenedores
+  let momentName = document.createTextNode(moment.moment);
+  let anime = document.createTextNode(moment.anime);
+  let card = document.createElement('div');
   let background = document.createElement('img');
   background.src = moment.image;
   let cardsDiv = document.querySelector('div.cards');

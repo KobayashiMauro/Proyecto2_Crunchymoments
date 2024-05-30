82c88867084374ee0331a886e71624495683956c
KobayashiMauro <auroravalentinivalea@icloud.com>
2024-05-30 21:23:53 +0200

    Conseguí terminar el hover bien y estoy cambiando la info de las tarjetas para que sean distintas

--- main.js
+++ main.js
@@ -1,51 +1,40 @@
 const moments = [
   {
-    moment: 'Law VS Kurohige',
-    anime: 'One Piece',
+    moment: 'Equipo Kakashi VS Sasori',
+    anime: 'Naruto Shippuden',
     characters: [
       {
-        first: 'Trafalgar D. Water Law',
-        second: 'Kurohige Marshall D. Teach',
+        first: 'Hatake Kakashi',
+        second: 'Uzumaki Naruto',
+        third: 'Sasori',
       },
     ],
-    episodes: 2,
-    duration: 48,
+    episodes: 24,
+    duration: 576,
     description:
-      'Law se enfrenta a Kurohige después de salir del país de Wano y de terminar su alianza con los Sombrero de Paja.',
-    image: 'assets/Lawcard.jpg',
+      'El equipo de Kakashi entra en acción cuando reciben la noticia de que su amigo Gaara, actual Kazekage, ha sido secuestrado por la organización Akatsuki.',
+    image: 'assets/Kakashicard.jpg',
     links: [
       {
-        first:
-          'https://www.crunchyroll.com/es-es/watch/GPWUKVW2E/bonneys-lamentation-darkness-lurking-on-the-future-island',
-        second: 'https://www.crunchyroll.com/es-es/watch/G8WUN19Q8/the-winner-takes-all-law-vs-blackbeard',
+        first: 'https://www.crunchyroll.com/es-es/watch/G63VGKZNY/team-kakashi-deployed',
       },
     ],
   },
   {
-    moment: 'El pasado de Law',
+    moment: 'Sulong de Carrot',
     anime: 'One Piece',
     characters: [
       {
-        first: 'Trafalgar D. Water Law',
-        second: 'Donquixote Doflamingo',
-        third: 'Donquixote Rosinante Corazon',
+        first: 'Carrot',
       },
     ],
-    episodes: 8,
-    duration: 192,
-    description:
-      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
-    image: 'assets/Lawminicard2.jpg',
+    episodes: 2,
+    duration: 48,
+    description: '¿Sabes cuántas veces ha entrado en acción la transformación Sulong de Carrot? ¡Compruébalo!',
+    image: 'assets/Carrotcard.jpg',
     links: [
       {
-        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
-        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
-        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
-        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
-        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
-        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
+        first: 'https://www.crunchyroll.com/es-es/watch/G6VD30QP6/sulong-carrots-big-mystic-transformation',
       },
     ],
   },
@@ -275,8 +264,8 @@ for (const moment of moments) {
   let momentName = document.createTextNode(moment.moment);
   let momentNameHover = document.createTextNode(moment.moment);
   let anime = document.createTextNode(moment.anime);
-  let momentEps = document.createTextNode(moment.episodes);
-  let momentDuration = document.createTextNode(moment.duration);
+  let momentEps = document.createTextNode('Epidosdios: ' + moment.episodes);
+  let momentDuration = document.createTextNode('Duración: ' + moment.duration + ' min');
   let momentDescription = document.createTextNode(moment.description);
 
   //Creo el contenedor de la tarjeta
@@ -298,33 +287,58 @@ for (const moment of moments) {
   //Dentro de <div> "cardHover":
   let backgroundHover = document.createElement('img');
   backgroundHover.src = moment.image;
+
+  let divHoverText = document.createElement('div');
+  divHoverText.classList.add('momentHover-text');
+  let divHoverShadow = document.createElement('div');
   let pEps = document.createElement('p');
   let pDuration = document.createElement('p');
   let pDescription = document.createElement('p');
   let h3Hover = document.createElement('h3');
+
+  let divHoverIcons = document.createElement('div');
+  divHoverIcons.classList.add('momentHover-icons');
+  let buttonPlay = document.createElement('button');
+  let hoverIconPlay = document.createElement('img');
+  hoverIconPlay.src = './assets/PNGNaranjaPlay.png';
+  let buttonFav = document.createElement('button');
+  let hoverIconFav = document.createElement('img');
+  hoverIconFav.src = './assets/PNGNaranjaFav.png';
+  let buttonAdd = document.createElement('button');
+  let hoverIconAdd = document.createElement('img');
+  hoverIconAdd.src = './assets/PNGNaranjaAdd.png';
 
   //Dónde lo voy a meter
   let cardsDiv = document.querySelector('div.cards');
   let cardsArt = card;
 
   //Pinto los datos
-  cardsDiv.appendChild(card); //article en div
-  card.appendChild(aImg); //enlace imagen en article
-  aImg.appendChild(background); //imagen en enlace imagen
+  cardsDiv.appendChild(aImg); //article en div
+  aImg.appendChild(card); //enlace imagen en article
+  card.appendChild(background); //imagen en enlace imagen
   card.appendChild(cardH3); //H3 en enlace H3
   cardH3.appendChild(momentName); //Texto en H3
   card.appendChild(cardH4); //H4 en enlace H4
   cardH4.appendChild(anime); //Texto en H4
 
   // Pinto los datos del hover
-  cardsArt.appendChild(cardHover); //div en enlace
-  cardHover.appendChild(backgroundHover); //imagen en div
+  aImg.appendChild(cardHover); //div en enlace
+  cardHover.appendChild(divHoverShadow); //div de shadow en div
+  divHoverShadow.appendChild(backgroundHover); //imagen en div de shadow
   h3Hover.appendChild(momentNameHover); //nombre en h3
-  cardHover.appendChild(h3Hover); //h3 en div
-  cardHover.appendChild(pEps); //p en div
-  cardHover.appendChild(pDuration); //p en div
-  cardHover.appendChild(pDescription); //p en div
+  cardHover.appendChild(divHoverText); // div de texto en div
+  divHoverText.appendChild(h3Hover); //h3 en div de texto
+  divHoverText.appendChild(pEps); //p en div de texto
+  divHoverText.appendChild(pDuration); //p en div de texto
+  divHoverText.appendChild(pDescription); //p en div de texto
   pEps.appendChild(momentEps); //episodios en p
   pDuration.appendChild(momentDuration); //duración en p
   pDescription.appendChild(momentDescription); //descripción en p
+  cardHover.appendChild(divHoverIcons); //div de iconos en div
+  divHoverIcons.appendChild(buttonPlay);
+  buttonPlay.appendChild(hoverIconPlay);
+  divHoverIcons.appendChild(buttonFav);
+  buttonFav.appendChild(hoverIconFav);
+  divHoverIcons.appendChild(buttonAdd);
+  buttonAdd.appendChild(hoverIconAdd);
 }

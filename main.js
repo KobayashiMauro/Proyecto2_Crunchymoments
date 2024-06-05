74b8b751c2fb4a1dc23225bd4dfc2b90035d1c9b
KobayashiMauro <auroravalentinivalea@icloud.com>
2024-06-05 20:46:27 +0200

    He creado el formulario para los filtros en HTML y he empezado con la funcionalidad, de momento puedo sacar el value del input por consola

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const moments = [
+  //Kakashi
   {
     moment: 'Equipo Kakashi VS Sasori',
     anime: 'Naruto Shippuden',
@@ -20,6 +21,7 @@ const moments = [
       },
     ],
   },
+  //Carrot
   {
     moment: 'Sulong de Carrot',
     anime: 'One Piece',
@@ -38,90 +40,7 @@ const moments = [
       },
     ],
   },
-  {
-    moment: 'El pasado de Law',
-    anime: 'One Piece',
-    characters: [
-      {
-        first: 'Trafalgar D. Water Law',
-        second: 'Donquixote Doflamingo',
-        third: 'Donquixote Rosinante Corazon',
-      },
-    ],
-    episodes: 8,
-    duration: 192,
-    description:
-      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
-    image: 'assets/Lawminicard2.jpg',
-    links: [
-      {
-        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
-        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
-        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
-        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
-        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
-        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
-      },
-    ],
-  },
-  {
-    moment: 'El pasado de Law',
-    anime: 'One Piece',
-    characters: [
-      {
-        first: 'Trafalgar D. Water Law',
-        second: 'Donquixote Doflamingo',
-        third: 'Donquixote Rosinante Corazon',
-      },
-    ],
-    episodes: 8,
-    duration: 192,
-    description:
-      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
-    image: 'assets/Lawminicard2.jpg',
-    links: [
-      {
-        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
-        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
-        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
-        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
-        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
-        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
-      },
-    ],
-  },
-  {
-    moment: 'El pasado de Law',
-    anime: 'One Piece',
-    characters: [
-      {
-        first: 'Trafalgar D. Water Law',
-        second: 'Donquixote Doflamingo',
-        third: 'Donquixote Rosinante Corazon',
-      },
-    ],
-    episodes: 8,
-    duration: 192,
-    description:
-      '¡El pasado de Law se revela! La motivación de Law para acabar con Doflamingo y el recuerdo de Corazón.',
-    image: 'assets/Lawminicard2.jpg',
-    links: [
-      {
-        first: 'https://www.crunchyroll.com/es-es/watch/G62P5G3K6/the-ultimate-power-the-secret-of-the-op-op-fruit',
-        second: 'https://www.crunchyroll.com/es-es/watch/G6ZX4ENXR/sad-memories-law-the-boy-from-the-white-town',
-        third: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fourth: 'https://www.crunchyroll.com/es-es/watch/GYQ49DV96/a-celestial-dragon-doffys-stormy-past',
-        fifth: 'https://www.crunchyroll.com/es-es/watch/GRWEMW72R/a-rocky-road-law-and-corazons-journey-of-life',
-        sixth: 'https://www.crunchyroll.com/es-es/watch/G60975NM6/the-time-is-ticking-down-seize-the-op-op-fruit',
-        seventh: 'https://www.crunchyroll.com/es-es/watch/G6751XE4R/the-moment-of-resolution-corazons-farewell-smile',
-        eighth: 'https://www.crunchyroll.com/es-es/watch/GYX0MGX5R/advance-law-the-kindhearted-mans-final-fight',
-      },
-    ],
-  },
+  //Law
   {
     moment: 'Law VS Kurohige',
     anime: 'One Piece',
@@ -141,6 +60,64 @@ const moments = [
         first:
           'https://www.crunchyroll.com/es-es/watch/GPWUKVW2E/bonneys-lamentation-darkness-lurking-on-the-future-island',
         second: 'https://www.crunchyroll.com/es-es/watch/G8WUN19Q8/the-winner-takes-all-law-vs-blackbeard',
+      },
+    ],
+  },
+  //Tengen
+  {
+    moment: 'Tengen Uzui VS Gyutaro',
+    anime: 'Kimetsu no Yaiba',
+    characters: [
+      {
+        first: 'Tengen Uzui',
+      },
+    ],
+    episodes: 4,
+    duration: 96,
+    description:
+      'Cuando parecía que estaba ya todo zanjado... Daki se divide en dos demonios y ahora Uzui tiene que lidiar con su hermano mayor Gyutaro.',
+    image: 'assets/Tengencard.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G2XU03NP8/gathering',
+      },
+    ],
+  },
+  //Gojo
+  {
+    moment: 'Satoru Gojo, y ya',
+    anime: 'Jujutsu Kaisen',
+    characters: [
+      {
+        first: 'Satoru Gojo',
+      },
+    ],
+    episodes: 7,
+    duration: 168,
+    description: 'No necesita mucho para lucirse, pero ¿cuántas veces ha sido el absoluto protagonista del momento?',
+    image: 'assets/Gojocard.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/G64G2V2VR/for-myself',
+      },
+    ],
+  },
+  //Zoro
+  {
+    moment: 'Zoro VS King',
+    anime: 'One Piece',
+    characters: [
+      {
+        first: 'Roronoa Zoro',
+      },
+    ],
+    episodes: 5,
+    duration: 120,
+    description: 'Por fin un digno rival para Zoro, una de las batallas más épicas de Wano',
+    image: 'assets/Zorocard.jpg',
+    links: [
+      {
+        first: 'https://www.crunchyroll.com/es-es/watch/GEVUZNJN3/zoro-faces-adversity---a-monster-king-the-wildfire',
       },
     ],
   },
@@ -259,86 +236,99 @@ const moments = [
 ];
 /* Print cards --------------------------------------------- */
 
-for (const moment of moments) {
-  //Me traigo los datos visibles
-  let momentName = document.createTextNode(moment.moment);
-  let momentNameHover = document.createTextNode(moment.moment);
-  let anime = document.createTextNode(moment.anime);
-  let momentEps = document.createTextNode('Epidosdios: ' + moment.episodes);
-  let momentDuration = document.createTextNode('Duración: ' + moment.duration + ' min');
-  let momentDescription = document.createTextNode(moment.description);
+function printCards(param) {
+  for (const moment of moments) {
+    //Me traigo los datos visibles
+    let momentName = document.createTextNode(moment.moment);
+    let momentNameHover = document.createTextNode(moment.moment);
+    let anime = document.createTextNode(moment.anime);
+    let momentEps = document.createTextNode('Episodios: ' + moment.episodes);
+    let momentDuration = document.createTextNode('Duración: ' + moment.duration + ' min');
+    let momentDescription = document.createTextNode(moment.description);
 
-  //Creo el contenedor de la tarjeta
-  let card = document.createElement('article');
-  card.classList.add('moment');
+    //Creo el contenedor de la tarjeta
+    let card = document.createElement('article');
+    card.classList.add('moment');
 
-  //Creo el contenedor de la tarjeta:hover
-  let cardHover = document.createElement('div');
-  cardHover.classList.add('momentHover');
+    //Creo el contenedor de la tarjeta:hover
+    let cardHover = document.createElement('div');
+    cardHover.classList.add('momentHover');
 
-  //Dentro de <article> "card":
-  let background = document.createElement('img');
-  background.src = moment.image;
-  let aImg = document.createElement('a');
-  aImg.href = moment.links[0].first;
-  let cardH3 = document.createElement('h3');
-  let cardH4 = document.createElement('h4');
+    //Dentro de <article> "card":
+    let background = document.createElement('img');
+    background.src = moment.image;
+    let aImg = document.createElement('a');
+    aImg.href = moment.links[0].first;
+    let cardH3 = document.createElement('h3');
+    let cardH4 = document.createElement('h4');
 
-  //Dentro de <div> "cardHover":
-  let backgroundHover = document.createElement('img');
-  backgroundHover.src = moment.image;
+    //Dentro de <div> "cardHover":
+    let backgroundHover = document.createElement('img');
+    backgroundHover.src = moment.image;
 
-  let divHoverText = document.createElement('div');
-  divHoverText.classList.add('momentHover-text');
-  let divHoverShadow = document.createElement('div');
-  let pEps = document.createElement('p');
-  let pDuration = document.createElement('p');
-  let pDescription = document.createElement('p');
-  let h3Hover = document.createElement('h3');
+    let divHoverText = document.createElement('div');
+    divHoverText.classList.add('momentHover-text');
+    let divHoverShadow = document.createElement('div');
+    let pEps = document.createElement('p');
+    let pDuration = document.createElement('p');
+    let pDescription = document.createElement('p');
+    let h3Hover = document.createElement('h3');
 
-  let divHoverIcons = document.createElement('div');
-  divHoverIcons.classList.add('momentHover-icons');
-  let buttonPlay = document.createElement('button');
-  let hoverIconPlay = document.createElement('img');
-  hoverIconPlay.src = './assets/PNGNaranjaPlay.png';
-  let buttonFav = document.createElement('button');
-  let hoverIconFav = document.createElement('img');
-  hoverIconFav.src = './assets/PNGNaranjaFav.png';
-  let buttonAdd = document.createElement('button');
-  let hoverIconAdd = document.createElement('img');
-  hoverIconAdd.src = './assets/PNGNaranjaAdd.png';
+    let divHoverIcons = document.createElement('div');
+    divHoverIcons.classList.add('momentHover-icons');
+    let buttonPlay = document.createElement('button');
+    let hoverIconPlay = document.createElement('img');
+    hoverIconPlay.src = './assets/PNGNaranjaPlay.png';
+    let buttonFav = document.createElement('button');
+    let hoverIconFav = document.createElement('img');
+    hoverIconFav.src = './assets/PNGNaranjaFav.png';
+    let buttonAdd = document.createElement('button');
+    let hoverIconAdd = document.createElement('img');
+    hoverIconAdd.src = './assets/PNGNaranjaAdd.png';
 
-  //Dónde lo voy a meter
-  let cardsDiv = document.querySelector('div.cards');
-  let cardsArt = card;
+    //Dónde lo voy a meter
+    let cardsDiv = document.querySelector('div.cards');
+    let cardsArt = card;
 
-  //Pinto los datos
-  cardsDiv.appendChild(aImg); //article en div
-  aImg.appendChild(card); //enlace imagen en article
-  card.appendChild(background); //imagen en enlace imagen
-  card.appendChild(cardH3); //H3 en enlace H3
-  cardH3.appendChild(momentName); //Texto en H3
-  card.appendChild(cardH4); //H4 en enlace H4
-  cardH4.appendChild(anime); //Texto en H4
+    //Pinto los datos
+    cardsDiv.appendChild(aImg); //article en div
+    aImg.appendChild(card); //enlace imagen en article
+    card.appendChild(background); //imagen en enlace imagen
+    card.appendChild(cardH3); //H3 en enlace H3
+    cardH3.appendChild(momentName); //Texto en H3
+    card.appendChild(cardH4); //H4 en enlace H4
+    cardH4.appendChild(anime); //Texto en H4
 
-  // Pinto los datos del hover
-  aImg.appendChild(cardHover); //div en enlace
-  cardHover.appendChild(divHoverShadow); //div de shadow en div
-  divHoverShadow.appendChild(backgroundHover); //imagen en div de shadow
-  h3Hover.appendChild(momentNameHover); //nombre en h3
-  cardHover.appendChild(divHoverText); // div de texto en div
-  divHoverText.appendChild(h3Hover); //h3 en div de texto
-  divHoverText.appendChild(pEps); //p en div de texto
-  divHoverText.appendChild(pDuration); //p en div de texto
-  divHoverText.appendChild(pDescription); //p en div de texto
-  pEps.appendChild(momentEps); //episodios en p
-  pDuration.appendChild(momentDuration); //duración en p
-  pDescription.appendChild(momentDescription); //descripción en p
-  cardHover.appendChild(divHoverIcons); //div de iconos en div
-  divHoverIcons.appendChild(buttonPlay);
-  buttonPlay.appendChild(hoverIconPlay);
-  divHoverIcons.appendChild(buttonFav);
-  buttonFav.appendChild(hoverIconFav);
-  divHoverIcons.appendChild(buttonAdd);
-  buttonAdd.appendChild(hoverIconAdd);
+    // Pinto los datos del hover
+    aImg.appendChild(cardHover); //div en enlace
+    cardHover.appendChild(divHoverShadow); //div de shadow en div
+    divHoverShadow.appendChild(backgroundHover); //imagen en div de shadow
+    h3Hover.appendChild(momentNameHover); //nombre en h3
+    cardHover.appendChild(divHoverText); // div de texto en div
+    divHoverText.appendChild(h3Hover); //h3 en div de texto
+    divHoverText.appendChild(pEps); //p en div de texto
+    divHoverText.appendChild(pDuration); //p en div de texto
+    divHoverText.appendChild(pDescription); //p en div de texto
+    pEps.appendChild(momentEps); //episodios en p
+    pDuration.appendChild(momentDuration); //duración en p
+    pDescription.appendChild(momentDescription); //descripción en p
+    cardHover.appendChild(divHoverIcons); //div de iconos en div
+    divHoverIcons.appendChild(buttonPlay);
+    buttonPlay.appendChild(hoverIconPlay);
+    divHoverIcons.appendChild(buttonFav);
+    buttonFav.appendChild(hoverIconFav);
+    divHoverIcons.appendChild(buttonAdd);
+    buttonAdd.appendChild(hoverIconAdd);
+  }
 }
+
+printCards(moments);
+
+let searchButton = document.getElementById('form-simple-button');
+let searchBar = document.getElementById('search-text');
+
+searchButton.onclick = function searchEvent(event) {
+  event.preventDefault();
+  let searchInputText = searchBar.value;
+  console.log(searchInputText);
+};
